55142a23849bd4a9182840d789413239955a1278
Wayne Werner <wayne@waynewerner.com>
2022-01-03 04:39:30 +0000

    better release script

--- release.sh
+++ release.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
-python3 setup.py bdist_wheel
+python3 test_shibboleth.py
+if [ $? -ne 0 ]; then
+    echo "Tests failed"
+    exit
+fi
+pyproject-build
 twine check dist/shibboleth-$(python3 shibboleth.py version)-py3-none-any.whl
-git tag $(python3 shibboleth.py version)
+git tag -a $(python3 shibboleth.py version)
 twine upload --config-file $HOME/.pypirc -u __token__ -r shibboleth dist/shibboleth-$(python3 shibboleth.py version)-py3-none-any.whl
