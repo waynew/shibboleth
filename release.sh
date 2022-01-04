dcd48634a23230e78a36b27aeeb68ab0acc99387
Wayne Werner <wayne@waynewerner.com>
2022-01-03 19:18:50 -0600

    shibboleth EOF

--- release.sh
+++ release.sh
@@ -5,7 +5,5 @@ if [ $? -ne 0 ]; then
     echo "Tests failed"
     exit
 fi
-pyproject-build
 twine check dist/shibboleth-$(python3 shibboleth.py version)-py3-none-any.whl
-git tag -a $(python3 shibboleth.py version)
 twine upload --config-file $HOME/.pypirc -u __token__ -r shibboleth dist/shibboleth-$(python3 shibboleth.py version)-py3-none-any.whl
---
+++ test_release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+python3 test_shibboleth.py
+if [ $? -ne 0 ]; then
+    echo "Tests failed"
+    exit
+fi
+#pyproject-build
+#twine check dist/shibboleth-$(python3 shibboleth.py version)-py3-none-any.whl
+#git tag -a $(python3 shibboleth.py version)
+twine upload --config-file $HOME/.pypirc -u __token__ -r test_shibboleth dist/shibboleth-$(python3 shibboleth.py version)-py3-none-any.whl --verbose
