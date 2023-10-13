ec21972bbe3d6831ffef32125c79d16e2ea86b4e
Wayne Werner <wayne@waynewerner.com>
2023-10-13 03:30:44 +0000

    update release script
    It should be possible to alias python as python3 on platforms where
    that's a problem.

--- test_release.sh
+++ test_release.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
-python3 test_shibboleth.py
+python test_shibboleth.py
 if [ $? -ne 0 ]; then
     echo "Tests failed"
     exit
 fi
 python -m build
 twine check dist/shibboleth-$(python shibboleth.py version)-py3-none-any.whl
-git tag -a $(python3 shibboleth.py version)
-twine upload --config-file $HOME/.pypirc -u __token__ -r test_shibboleth dist/shibboleth-$(python3 shibboleth.py version)-py3-none-any.whl --verbose
+git tag -a $(python shibboleth.py version)
+twine upload --config-file $HOME/.pypirc -u __token__ -r test_shibboleth dist/shibboleth-$(python shibboleth.py version)-py3-none-any.whl --verbose
