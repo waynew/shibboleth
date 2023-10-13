08deb741a5b3ca59e4cc6d98c519e5720dc3278e
Wayne Werner <wayne@waynewerner.com>
2023-10-13 03:49:20 +0000

    there can be only one... release.sh

--- release.sh
+++ release.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
-python3 test_shibboleth.py
+python test_shibboleth.py
 if [ $? -ne 0 ]; then
     echo "Tests failed"
     exit
 fi
-twine check dist/shibboleth-$(python3 shibboleth.py version)-py3-none-any.whl
-twine upload --config-file $HOME/.pypirc -u __token__ -r shibboleth dist/shibboleth-$(python3 shibboleth.py version)-py3-none-any.whl
+python -m build
+twine check dist/shibboleth-$(python shibboleth.py version)-py3-none-any.whl
+git tag -a $(python shibboleth.py version)
+twine upload --config-file $HOME/.pypirc -u __token__ -r test_shibboleth dist/shibboleth-$(python shibboleth.py version)-py3-none-any.whl --verbose
