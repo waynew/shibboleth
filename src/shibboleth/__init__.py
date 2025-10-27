7030442093252e3250da43c60cbfb620eb1c2f84
Wayne Werner <wayne@waynewerner.com>
2025-10-26 22:51:42 -0500

    Release 25.10.2

--- src/shibboleth/__init__.py
+++ src/shibboleth/__init__.py
@@ -19,7 +19,7 @@ from textwrap import dedent
 
 logger = logging.getLogger(__name__)
 
-__version__ = "25.10.0"
+__version__ = "25.10.1"
 
 HIDDEN_FILES = (".last.shib", ".gitignore", "shibboleth.log")
 DEFAULT_COLORS = {
